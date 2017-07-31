7db1da115aaa8e559120340a303a1560dcde42ce
N Oliver <github@pixnbits.org>
2017-07-30 23:12:12 -0700

    feat(Quaternion): ported constructor & methods

---
+++ test/Quaternion.spec.js
@@ -0,0 +1,21 @@
+describe('Quaternion', () => {
+  describe('constructor', () => {
+    it('from scalars');
+    it('from HParams');
+    it('from vectors');
+    it('from axis & angle');
+  });
+  it('plus');
+  it('minus');
+  it('scaledBy');
+  it('magnitude');
+  it('withMagnitude');
+  it('rotationU');
+  it('rotationV');
+  it('rotationN');
+  it('rotate');
+  it('toThe');
+  it('inverse');
+  it('times');
+  it('mirror');
+});
